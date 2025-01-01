5604a78a6420f6b60f9f521dc9c7d6aa3a9aaf7d
Peomarder <36908523+Peomarder@users.noreply.github.com>
2025-01-01 21:10:25 +0700

    Update s2_a1.cpp

--- s2_a1.cpp
+++ s2_a1.cpp
@@ -7,71 +7,66 @@
 #include <fstream>
 
 std::string testBackendFunction(const std::string& username, const std::string& password, const std::string& message) {
-    return "User: " + username + " sent message: " + message;
+return "User: " + username + " sent message: " + message;
 }
 
 std::string processRequest(const char* request) {
-    std::string req(request);
-    size_t firstSpace = req.find(' ');
-    size_t secondSpace = req.find(' ', firstSpace + 1);
+std::string req(request);
+size_t firstSpace = req.find(' ');
+size_t secondSpace = req.find(' ', firstSpace + 1);
 
-    if(firstSpace == std::string::npos || secondSpace == std::string::npos) {
-        return "Invalid format";
-    }
+if(firstSpace == std::string::npos || secondSpace == std::string::npos) {
+return "Invalid format";
+}
 
-    std::string username = req.substr(0, firstSpace);
-    std::string password = req.substr(firstSpace + 1, secondSpace - firstSpace - 1);
-    std::string message = req.substr(secondSpace + 1);
+std::string username = req.substr(0, firstSpace);
+std::string password = req.substr(firstSpace + 1, secondSpace - firstSpace - 1);
+std::string message = req.substr(secondSpace + 1);
 
-    return testBackendFunction(username, password, message);
+return testBackendFunction(username, password, message);
 }
 
 int main() {
-    const int PORT = 7432;
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    
-    if (serverSocket < 0) {
-        std::cout << "Socket creation failed\n";
-        return -1;
-    }
+const int PORT = 7432;
+int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    // Get and display server IP
-    char hostBuffer[256];
-    gethostname(hostBuffer, sizeof(hostBuffer));
-    struct hostent *host_entry = gethostbyname(hostBuffer);
-    char *serverIP = inet_ntoa(*((struct in_addr*)host_entry->h_addr_list[0]));
-    std::cout << "Server IP: " << serverIP << "\nPORT: " << PORT << std::endl;
+if (serverSocket < 0) {
+std::cout << "Socket creation failed" << std::endl;
+return -1;
+}
 
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
-    serverAddress.sin_port = htons(PORT);
+struct sockaddr_in serverAddress;
+serverAddress.sin_family = AF_INET;
+serverAddress.sin_addr.s_addr = INADDR_ANY;
+serverAddress.sin_port = htons(PORT);
 
-    if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
-        std::cout << "Binding failed\n";
-        return -1;
-    }
+std::cout << "Server PORT: " << PORT << std::endl;
 
-    listen(serverSocket, 3);
-    std::cout << "Server is listening...\n";
+if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+std::cout << "Binding failed" << std::endl;
+return -1;
+}
 
-    while (true) {
-        sockaddr_in clientAddress;
-        socklen_t clientLength = sizeof(clientAddress);
-        int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientLength);
+listen(serverSocket, 3);
+std::cout << "Server is listening..." << std::endl;
 
-        std::cout << "New connection from: " 
-                  << inet_ntoa(clientAddress.sin_addr) 
-                  << ":" << ntohs(clientAddress.sin_port) << std::endl;
+while (true) {
+sockaddr_in clientAddress;
+socklen_t clientLength = sizeof(clientAddress);
+int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientLength);
 
-        char buffer[1024] = {0};
-        read(clientSocket, buffer, 1024);
-        std::cout << "Received: " << buffer << std::endl;
+std::cout << "New connection from: "
+<< inet_ntoa(clientAddress.sin_addr)
+<< ":" << ntohs(clientAddress.sin_port) << std::endl;
 
-        std::string response = processRequest(buffer);
-        send(clientSocket, response.c_str(), response.length(), 0);
-        close(clientSocket);
-    }
+char buffer[1024] = {0};
+read(clientSocket, buffer, 1024);
+std::cout << "Received: " << buffer << std::endl;
 
-    return 0;
+std::string response = processRequest(buffer);
+send(clientSocket, response.c_str(), response.length(), 0);
+close(clientSocket);
+}
+
+return 0;
 }
