858e8ac7adab54fa6b65ac712e9fc13333676d6c
Peomarder <36908523+Peomarder@users.noreply.github.com>
2025-01-12 16:13:58 +0700

    Update s.cpp

--- s.cpp
+++ s.cpp
@@ -20,146 +20,146 @@ mutex mtx;
 
 string testBackendFunction(const string& username, const string& password, const string& message) {
 	cout<<"\n test";
-return "User: " + username + " sent message: \n" + subp(message);
+	return "User: " + username + " sent message: \n" + subp(message);
 }
 
 string processRequest(const char* request) {
-string req(request);
-size_t firstSpace = req.find(' ');
-size_t secondSpace = req.find(' ', firstSpace + 1);
+	string req(request);
+	size_t firstSpace = req.find(' ');
+	size_t secondSpace = req.find(' ', firstSpace + 1);
 
-if(firstSpace == string::npos || secondSpace == string::npos) {
-return "Invalid format";
-}
+	if(firstSpace == string::npos || secondSpace == string::npos) {
+		return "Invalid format";
+	}
 
-string username = req.substr(0, firstSpace);
-string password = req.substr(firstSpace + 1, secondSpace - firstSpace - 1);
-string message = req.substr(secondSpace + 1);
+	string username = req.substr(0, firstSpace);
+	string password = req.substr(firstSpace + 1, secondSpace - firstSpace - 1);
+	string message = req.substr(secondSpace + 1);
 
-return testBackendFunction(username, password, message);
+	return testBackendFunction(username, password, message);
 }
 
 void handleClient(int clientSocket) {
-{
-lock_guard<mutex> lock(mtx);
-cout << "Handling client in thread... Socket: " << clientSocket << endl;
-}
+	{
+		lock_guard<mutex> lock(mtx);
+		cout << "Handling client in thread... Socket: " << clientSocket << endl;
+	}
 
-while(true) {
-cout << "Waiting for message..." << endl;  // Debug print
-char buffer[1024] = {0};
-int bytesRead = read(clientSocket, buffer, 1024);
+	while(true) {
+		cout << "Waiting for message..." << endl;  // Debug print
+		char buffer[1024] = {0};
+		int bytesRead = read(clientSocket, buffer, 1024);
 
-cout << "Bytes read: " << bytesRead << endl;  // Debug print
+		cout << "Bytes read: " << bytesRead << endl;  // Debug print
 
-if(bytesRead <= 0) {
-lock_guard<mutex> lock(mtx);
-cout << "Client disconnected (bytes: " << bytesRead << ")" << endl;
-break;
-}
+		if(bytesRead <= 0) {
+			lock_guard<mutex> lock(mtx);
+			cout << "Client disconnected (bytes: " << bytesRead << ")" << endl;
+			break;
+		}
 
-{
-lock_guard<mutex> lock(mtx);
-cout << "Received: " << buffer << endl;
-}
+		{
+			lock_guard<mutex> lock(mtx);
+			cout << "Received: " << buffer << endl;
+		}
 
-string response = processRequest(buffer);
-cout << "Sending response..." << endl;  // Debug print
-send(clientSocket, response.c_str(), response.length(), 0);
-}
+		string response = processRequest(buffer);
+		cout << "Sending response..." << endl;  // Debug print
+		send(clientSocket, response.c_str(), response.length(), 0);
+	}
 
-close(clientSocket);
+	close(clientSocket);
 }
 
 int main() {
-const int PORT = 7432;
-int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+	const int PORT = 7432;
+	int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-if (serverSocket < 0) {
-cout << "Socket creation failed" << endl;
-return -1;
-}
+	if (serverSocket < 0) {
+	cout << "Socket creation failed" << endl;
+	return -1;
+	}
 
-struct sockaddr_in serverAddress;
-serverAddress.sin_family = AF_INET;
-serverAddress.sin_addr.s_addr = INADDR_ANY;
-serverAddress.sin_port = htons(PORT);
+	struct sockaddr_in serverAddress;
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.s_addr = INADDR_ANY;
+	serverAddress.sin_port = htons(PORT);
 
-cout << "Server PORT: " << PORT << endl;
+	cout << "Server PORT: " << PORT << endl;
 
-struct ifreq ifr;
-ifr.ifr_addr.sa_family = AF_INET;
-strncpy(ifr.ifr_name, "eth0", IFNAMSIZ-1);
-ioctl(serverSocket, SIOCGIFADDR, &ifr);
+	struct ifreq ifr;
+	ifr.ifr_addr.sa_family = AF_INET;
+	strncpy(ifr.ifr_name, "eth0", IFNAMSIZ-1);
+	ioctl(serverSocket, SIOCGIFADDR, &ifr);
 
-cout << "Server IP: " 
-<< inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr) 
-<< endl;
+	cout << "Server IP: " 
+	<< inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr) 
+	<< endl;
 
-int reuseAddr = 1;
-setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &reuseAddr, sizeof(reuseAddr));
+	int reuseAddr = 1;
+	setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &reuseAddr, sizeof(reuseAddr));
 
-if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
-cout << "Binding failed" << endl;
-return -1;
-}
+	if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+	cout << "Binding failed" << endl;
+	return -1;
+	}
 
-listen(serverSocket, 5);
-cout << "Server is listening..." << endl;
+	listen(serverSocket, 5);
+	cout << "Server is listening..." << endl;
 
-/// ///////////
+	/// ///////////
 
-// Получим IP
-char actualIP[INET_ADDRSTRLEN];
-struct sockaddr_in actualAddr;
-socklen_t len = sizeof(actualAddr);
-
-
-char hostname[1024];
-gethostname(hostname, 1024);
-cout << "Hostname: " << hostname << endl;
+	// Получим IP
+	char actualIP[INET_ADDRSTRLEN];
+	struct sockaddr_in actualAddr;
+	socklen_t len = sizeof(actualAddr);
 
 
-hostent* he;
-he = gethostbyname(hostname);  // hostname - имя вашей машины
-
-if (he == NULL) {
-    cout << "Failed to get host name" << endl;
-    return -1;
-}
-
-char* ip = inet_ntoa(*(struct in_addr*)he->h_addr_list[0]);
-cout << "Server is listening on IP: " << ip << endl;
-
-if (getsockname(serverSocket, (struct sockaddr*)&actualAddr, &len) == -1) {
-    cout << "Failed to get socket address" << endl;
-    return -1;
-}
+	char hostname[1024];
+	gethostname(hostname, 1024);
+	cout << "Hostname: " << hostname << endl;
 
 
-/// /////////////////
+	hostent* he;
+	he = gethostbyname(hostname);  // hostname - имя вашей машины
 
-while (true) {
-    sockaddr_in clientAddress;
-    socklen_t clientLength = sizeof(clientAddress);
-    int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientLength);
+	if (he == NULL) {
+		cout << "Failed to get host name" << endl;
+		return -1;
+	}
 
-    if (clientSocket == -1) {
-        cerr << "Error occurred while connecting to client!" << endl;
-        continue;
-    }
+	char* ip = inet_ntoa(*(struct in_addr*)he->h_addr_list[0]);
+	cout << "Server is listening on IP: " << ip << endl;
 
-    {
-        lock_guard<mutex> lock(mtx);
-        cout << "New connection from: "
-                  << inet_ntoa(clientAddress.sin_addr)
-                  << ":" << ntohs(clientAddress.sin_port) << endl;
-		cout<<"\nDetaching...";
-    }
-	cout << "Handling client in thread... Socket: " << clientSocket << endl;
-    thread clientThread(handleClient, clientSocket);
-    clientThread.detach();
-}
+	if (getsockname(serverSocket, (struct sockaddr*)&actualAddr, &len) == -1) {
+		cout << "Failed to get socket address" << endl;
+		return -1;
+	}
+
+
+	/// /////////////////
+
+	while (true) {
+		sockaddr_in clientAddress;
+		socklen_t clientLength = sizeof(clientAddress);
+		int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientLength);
+
+		if (clientSocket == -1) {
+			cerr << "Error occurred while connecting to client!" << endl;
+			continue;
+		}
+
+		{
+			lock_guard<mutex> lock(mtx);
+			cout << "New connection from: "
+					  << inet_ntoa(clientAddress.sin_addr)
+					  << ":" << ntohs(clientAddress.sin_port) << endl;
+			cout<<"\nDetaching...";
+		}
+		cout << "Handling client in thread... Socket: " << clientSocket << endl;
+		thread clientThread(handleClient, clientSocket);
+		clientThread.detach();
+	}
 
 
 return 0;
